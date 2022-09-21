43eca06b08aec83f1b2c8a24682a03a5f61cdccd
Marco Tereso <marco.tereso@gmail.com>
2022-09-21 11:17:04 +0100

    Ficha3 - Ex1

---
+++ TI827ManipulaStrings/src/ti827manipulastrings/ExerciciosStrings.java
@@ -0,0 +1,17 @@
+
+
+package ti827manipulastrings;
+
+
+class ExerciciosStrings {
+
+    static int contaCaracteres() {
+       String s,s1;
+       System.out.println("Insira um texto");
+       s = TI827ManipulaStrings.ler.next();
+       s1 =TI827ManipulaStrings.ler.nextLine();
+      // s = s+s1;
+       return s1.length();
+    }
+
+}
---
+++ TI827ManipulaStrings/src/ti827manipulastrings/TI827ManipulaStrings.java
@@ -0,0 +1,40 @@
+
+
+package ti827manipulastrings;
+
+import java.util.Scanner;
+
+
+public class TI827ManipulaStrings {
+
+    public static Scanner ler = new Scanner(System.in);
+    public static void main(String[] args) {
+       int op;
+      
+       do{
+           System.out.println(">>>>>>>>>>MENU<<<<<<<<<<");
+           System.out.println("1 - Tamanho String");
+           
+           System.out.println("0 - Sair");
+           System.out.println("Insira a opção pretendida");
+           op=ler.nextInt();
+           switch(op){
+               case 0: break;
+               case 1: 
+                   System.out.println("A string Inserida tem: "+
+                           ExerciciosStrings.contaCaracteres()+" caracteres"); 
+                   /*
+                   int t = ExerciciosStrings.contaCaracteres();
+                   System.out.println("A string Inserida tem: "+t+" caracteres");
+                   */
+                   
+                   
+                   
+                   break;
+               default: 
+                   System.out.println("Insira opção válida");
+           }
+       }while(op!=0);
+    }
+
+}
